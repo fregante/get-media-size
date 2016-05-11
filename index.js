d1047b96be7b5620501c5069e9e27959f51815ce
Federico Brigante <bfred-it@users.noreply.github.com>
2016-05-11 08:39:22 -0700

    Fixed missing media case; shorter code; removed warnings

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 'use strict';
-function warn (msg) {
-	if (window.console && window.console.warn) {
-		window.console.warn(msg);
-	}
-}
+
 /**
  * getMediaSize
  * @param  {image|video|canvas|context} media The element to read the size from
@@ -11,11 +7,8 @@ function warn (msg) {
  * @return {object}                           Contains width and height of the passed media
  */
 export default function (media, scale) {
-	let size;
-
 	if (!media) {
-		warn('Missing media');
-		size = {
+		return {
 			width: 0,
 			height: 0,
 		};
@@ -24,25 +17,16 @@ export default function (media, scale) {
 	if (media.canvas) { // it's a ctx
 		media = media.canvas;
 	}
-	if (media.getContext) { // it's a canvas
-		size = {
-			width: media.width,
-			height: media.height,
-		};
-	} else { // image or video
-		size = {
-			width: media.naturalWidth || media.videoWidth || 0,
-			height: media.naturalHeight || media.videoHeight || 0,
-		};
-	}
+	
+	const size = {
+		width: media.naturalWidth || media.videoWidth || media.width || 0,
+		height: media.naturalHeight || media.videoHeight || media.height || 0,
+	};
 
 	if (scale) {
 		size.width /= scale;
 		size.height /= scale;
 	}
-
-	if (!size.width || !size.height) {
-		warn('Media has no size');
-	}
+	
 	return size;
 }
